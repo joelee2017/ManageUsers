93e18427c65b23e3f44dfdb3ac3abfa9eb816a3b
joelee2017 <as955133@gmail.com>
2018-02-06 16:31:12 +0800

    加入專案檔案。

---
+++ ManageUsers/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ManageUsers.Startup))]
+namespace ManageUsers
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
